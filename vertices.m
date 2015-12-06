2a1c3caa12de8d0d3d6167ae67899b24492d599e
Joel Lopez <jowie_katy@hotmail.com>
2015-12-06 15:26:15 +0100

    First vertices count implementation

--- vertices.m
+++ vertices.m
@@ -1,19 +1,38 @@
 function count = vertices(img)
-    %img = imread('letters/7.png');
+    %img = imread('letters/9.png');
 
     im = rgb2gray(img);
     g = graythresh(im);
     imb = im2bw(im,g);
     imb = imclearborder(~imb);
     %imb = imopen(imb, strel('square', 3)); 
-    
-    imshow(imb)
 
     skel = bwmorph(imb, 'skel', Inf);
-
+    
+    count = 0;
+    
+    [w, h] = size(skel);
+    for i = 1:w
+        for j = 1:h
+            if isVertice(skel, i, j)
+                count = count + 1;
+            end
+        end
+    end
+    
     %imshow(skel);
+end
 
-    corn = corner(skel);
-
-    count = size(corn,1);
+function is = isVertice(skel, r, c)
+    is = skel(r,c);
+    if is
+        top = skel(max(r-1, 1), max(c-1,1):min(c+1,end));
+        mid = skel(r, max(c-1,1):min(c+1,end));
+        mid(2) = 0;
+        bot = skel(max(r+1, 1), max(c-1,1):min(c+1,end));
+        aT = any(top);
+        aM = sum(mid);
+        aB = any(bot);
+        is = (aT + aM + aB) == 1;
+    end
 end
